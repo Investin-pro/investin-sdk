82bedb959f917611f55a151f7c1ab9050da9d073
raashidjunaid1 <raashidjunaid1@gmail.com>
2022-02-01 13:03:03 +0530

    removed testing code

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,3 @@ export * from './tokens';
 export * from './coingeckoTokens';
 export * from './perpMarkets';
 export * from './investor';
-
-import { Connection, PublicKey } from '@solana/web3.js';
-import { InvestinClient } from "./investinClient";
-
-export const cluster = "https://investinpro.genesysgo.net"
-export const connection = new Connection(cluster, 'recent');
-
-const main = async() => {
-    const investinClient = new InvestinClient(connection);
-
-    await investinClient.fetchAllFunds();
-    const investments = await investinClient.getInvestmentsByInvestorAddress(new PublicKey('2iBo4Q9iX9zaJQBG87QLPc6aBM3N3mPBjg1bPr4S1pwp'));
-    console.log('investments ::: ', investments);
-};
-
-main()
