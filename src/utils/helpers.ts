a8c2778d7e4732211488c20be14b12b87f8185d5
getbold.eth <durbas92@gmail.com>
2022-03-25 17:21:19 +0100

    Add prices to list of fund tokens

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,10 @@
-export const mapTokens = (platformTokenList, tokens, TOKENS) => {
+export const mapTokens = (platformTokenList, tokens, TOKENS, prices) => {
     return tokens.filter(f => f.is_active == 1).map((t) => {
+        const symbol = Object.keys(TOKENS).find(k => TOKENS[k].mintAddress == platformTokenList[t.index[t.mux]].mint);
+        const tokenPrice = prices.find((token) => token.symbol.toLowerCase() === symbol.toLowerCase());
         return {
-            symbol: Object.keys(TOKENS).find(k => TOKENS[k].mintAddress == platformTokenList[t.index[t.mux]].mint),
+            symbol,
+            price: tokenPrice ? tokenPrice.price : 1.00,
             // mint: platformTokenList[t.index].mint, 
             mint: platformTokenList[t.index[t.mux]].mint,
             balance: t.balance - t.debt,
